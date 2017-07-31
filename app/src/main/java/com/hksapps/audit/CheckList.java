862e79cfacf0f27202152af9516e60b744ec7462
Kamal <test@youremail.com>
2017-07-31 17:52:23 +0530

    Checklist.java is done! Now implementing database handler

---
+++ app/src/main/java/com/hksapps/audit/CheckList.java
@@ -0,0 +1,76 @@
+package com.hksapps.audit;
+
+/**
+ * Created by Administrator on 7/31/2017.
+ */
+
+public class CheckList {
+    //private variables
+    int id;
+    String checklist, yes, no, remarks;
+
+    public CheckList(){
+
+    }
+
+    public CheckList(int id,String checklist,String yes ,String no , String remarks){
+
+        this.id = id;
+        this.checklist = checklist;
+        this.yes = yes;
+        this.no = no;
+        this.remarks = remarks;
+    }
+
+
+    // getting ID
+    public int getId(){
+        return this.id;
+    }
+
+    // setting id
+    public void setId(int id){
+        this.id = id;
+    }
+
+
+    public String getChecklist(){
+        return this.checklist;
+    }
+
+
+    public void setChecklist(String Checklist){
+        this.checklist = Checklist;
+    }
+
+
+
+    public String getYes(){
+        return this.yes;
+    }
+
+
+    public void setYes(String Yes){
+        this.yes = Yes;
+    }
+
+
+    public String getNo(){
+        return this.no;
+    }
+
+
+    public void setNo(String no){
+        this.no = no;
+    }
+
+
+    public String getRemarks(){
+        return this.remarks;
+    }
+
+
+    public void setRemarks(String remarks){
+        this.remarks = remarks;
+    }
+}
---
+++ app/src/main/java/com/hksapps/audit/DatabaseHandler.java
@@ -0,0 +1,8 @@
+package com.hksapps.audit;
+
+/**
+ * Created by Administrator on 7/31/2017.
+ */
+
+public class DatabaseHandler {
+}
