680766573abd64d210c1ef2add39a77fd131a1e8
Kamal <kamalakar.sambaraj@gmai.com>
2017-08-03 22:19:06 +0530

    Questions are stored in arraylist!

--- app/src/main/java/com/hksapps/audit/Questions.java
+++ app/src/main/java/com/hksapps/audit/Questions.java
@@ -3,8 +3,6 @@ package com.hksapps.audit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -57,62 +55,18 @@ public class Questions extends AppCompatActivity {
 
 
         Log.e("size check list ", String.valueOf(allChecklists.size()));
-        int i = 0 ;
+
          question_chk=new ArrayList<>();
         for(CheckList con : allChecklists) {
-            String temp= con.getChecklist();
-           question_chk.add(temp);
+            String temp = con.getChecklist();
+            question_chk.add(temp);
 
-//            temp =null;
         }
-       /* String txt="";
-        for(CheckList con : allChecklists){
-             txt = txt +con.getChecklist();
-            ques.setText(txt);
-        }*/
-
-//       for(int i=0;i<allChecklists.size();i++)
-//        {
-//
-//           String t = allChecklists.get(i);
-//
-//        }
-
-        ques.setText(question_chk.get(j-1));
-
-        Button next = (Button) findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(j>question_chk.size()-1){
-                    Toast.makeText(Questions.this, "All Questions done!", Toast.LENGTH_SHORT).show();
-                    j--;
-                }else {
-                    ques.setText(question_chk.get(j));
-                    Log.e("J value is", String.valueOf(j));
-                    if(j!=question_chk.size())
-                    j++;
-
-                }
-            }
-        });
 
 
-        Button previous = (Button) findViewById(R.id.previous);
-        previous.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(j<0){
-                    Toast.makeText(Questions.this, "This is first question", Toast.LENGTH_SHORT).show();
-                }else  {
-                    ques.setText(question_chk.get(j));
-                  if(j!=0)
-                    j--;
 
-                }
-            }
-        });
+
+
 
 
 
