0b55d984c0f7bbc3accd95b43ba7c268b92a4e6a
Kamal <test@youremail.com>
2017-08-22 15:08:43 +0530

    Created ReviewScreen Branch

---
+++ app/src/main/java/com/hksapps/audit/ReviewScreen.java
@@ -0,0 +1,13 @@
+package com.hksapps.audit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReviewScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_screen);
+    }
+}
